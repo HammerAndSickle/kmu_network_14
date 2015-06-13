7b69f2a14adcb12a2f4773068af55cd05da8b148
user <cardongmin@gmail.com>
2015-06-13 17:50:20 +0900

    구현했으나 파일 전송 에러. 문제점 알아봐주시길..

--- client.c
+++ client.c
@@ -7,9 +7,26 @@
 #include <string.h>
 #include <ctype.h>
 #include <unistd.h>
+#include <pthread.h>
 
 //
 #define DEFAULT_SPEED 20
+#define USABLE_THREADS 500
+#define MSGLEN 128
+#define FILE_BUFFER_SIZE 2000
+
+//스레드를 실행할 때, 매개변수로 넣을 구조체.
+typedef struct threadArgs
+{
+    char fname[MSGLEN];
+    int speed;
+    int portNum;
+    char serverIP[MSGLEN];
+    int* threadIdx;
+} threadArgs;
+
+void* ReceiveData(void* p);
+void* SendData(void* p);
 
 int main(int argc, char *argv[])
 {
@@ -20,9 +37,17 @@ int main(int argc, char *argv[])
     static struct timeval timeout = { 5, 0 }; /* five seconds */
     fd_set rmask, xmask, mask;
     char buf[BUFSIZ];
+    char fname[MSGLEN] = {0, };
     int nfound, bytesread;
     char tempStr[64] = {0, };
 
+    threadArgs args[USABLE_THREADS];   //arguments for thread
+    pthread_t threads[USABLE_THREADS]; //threads
+
+
+    int dataPort;         //데이터 전송용 포트는 명령어 포트에서 1씩 증가한 포트값을 사용
+    int threadIdx = 0;          //스레드의 인덱스(스레드가 공유함)
+    int threadIdxFIX;           //작업을 수행하다 인덱스가 바뀌면 안되니 고정
 
     //
     int getSpeed = DEFAULT_SPEED;
@@ -36,12 +61,13 @@ int main(int argc, char *argv[])
         perror("socket");
         exit(1);
     }
+
     if (isdigit(argv[1][0])) {
         static struct servent s;
         servp = &s;
         s.s_port = htons((u_short)atoi(argv[1]));
     } else if ((servp = getservbyname(argv[1], "tcp")) == 0) {
-        fprintf(stderr,"%s: unknown service\n",argv[1]);
+        fprintf(stderr,"%s: unknown service\n",atoi(argv[1]));
         exit(1);
     }
     if ((hostp = gethostbyname(argv[2])) == 0) {
@@ -84,8 +110,66 @@ int main(int argc, char *argv[])
 
             buf[(strlen(buf) - 1)] = '\0';
 
+            if (write(sock, buf, strlen(buf)) < 0) {
+                perror("write");
+                exit(1);
+            }
+
+            if(strncmp(buf, "put", 3) == 0)
+            {           
+                        //fix thread
+                        threadIdxFIX = threadIdx;
+
+                        strcpy(fname, buf + 4); //파일명 추출
+                        strcpy(args[threadIdxFIX].fname, fname);
+                        args[threadIdxFIX].threadIdx = &threadIdx;
+
+                        threadIdx++;
+
+                        //now, receive new portnum for file transfer
+                        bytesread = read(sock, buf, sizeof buf);
+                        buf[bytesread] = '\0';
+
+
+                        memcpy(&dataPort, (int*)buf, 4);
+                        args[threadIdxFIX].portNum = dataPort;
+                        strcpy(args[threadIdxFIX].serverIP, argv[2]);
+
+                        printf("new port for transfer : %d\n", dataPort);
+
+                        //run new thread
+                        pthread_create(&threads[threadIdxFIX], NULL, SendData, (void*)&args[threadIdxFIX]);
+                        
+            }
+
+            else if(strncmp(buf, "get", 3) == 0)
+            {
+                       //fix thread
+                        threadIdxFIX = threadIdx;
+
+                        strcpy(fname, buf + 4); //파일명 추출
+                        strcpy(args[threadIdxFIX].fname, fname);
+                        args[threadIdxFIX].threadIdx = &threadIdx;
+
+                        threadIdx++;
+
+                        //now, receive new portnum for file transfer
+                        bytesread = read(sock, buf, sizeof buf);
+                        buf[bytesread] = '\0';
+
+                        memcpy(&dataPort, (int*)buf, 4);
+                        args[threadIdxFIX].portNum = dataPort;
+                        strcpy(args[threadIdxFIX].serverIP, argv[2]);
+
+                        printf("new port for transfer : %d\n", dataPort);
+
+                        //run new thread
+                        pthread_create(&threads[threadIdxFIX], NULL, ReceiveData, (void*)&args[threadIdxFIX]);
+                
+            }
+
             //sendrate ***K (put)
-            if(strncmp(buf, "sendrate", 8) == 0)
+            else if(strncmp(buf, "sendrate", 8) == 0)
             {
                 strcpy(tempStr, buf + 9);  //extract number part
                 tempStr[(strlen(tempStr) - 1)] = '\0';  //throw away 'K';
@@ -94,7 +178,7 @@ int main(int argc, char *argv[])
             }
 
             //recvrate (get)
-            if(strncmp(buf, "recvrate", 8) == 0)
+            else if(strncmp(buf, "recvrate", 8) == 0)
             {
                 strcpy(tempStr, buf+ 9);
                 tempStr[(strlen(tempStr) - 1)] = '\0';  //throw away 'K';
@@ -103,15 +187,11 @@ int main(int argc, char *argv[])
             }
 
             //yout speeds
-            if(strncmp(buf, "ratecurr", 8) == 0)
+            else if(strncmp(buf, "ratecurr", 8) == 0)
             {
                 printf("send : %d, recv : %d\n", putSpeed, getSpeed);
             }
 
-            if (write(sock, buf, strlen(buf)) < 0) {
-                perror("write");
-                exit(1);
-            }
         }
         if (FD_ISSET(sock,&rmask)) {
             /* data from network */
@@ -121,3 +201,187 @@ int main(int argc, char *argv[])
         }
     }
 } /* main - client.c */
+
+
+
+
+//파일을 받는 함수 (아직 미구현 상태)
+void* ReceiveData(void* p)
+{
+    struct sockaddr_in servaddr;
+    int listen_sock, // 소켓번호
+    nbyte, nbuf;
+    char buf[FILE_BUFFER_SIZE];
+    char cli_ip[20];
+    char filename[20];
+    int filesize=0;
+    int total=0, sread;
+
+    FILE* fp;
+
+    char serverIP[MSGLEN] = {0, };
+
+    //unpack all datas
+    threadArgs* args = (threadArgs*)p;
+    int* threadIdx = NULL;
+    int portNum = args->portNum;
+    int BLOCK = args->speed; 
+
+    strcpy(filename, args->fname); //filename
+    strcpy(serverIP, args->serverIP);
+    threadIdx = args->threadIdx;
+
+    //nKB means 1024*n Bytes
+    BLOCK = 1024 * BLOCK;
+
+
+
+    if((listen_sock = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+    {
+     perror("socket fail");
+     exit(0);
+     }
+ 
+     // 에코 서버의 소켓주소 구조체 작성
+     bzero((char *)&servaddr, sizeof(servaddr));
+     servaddr.sin_family = AF_INET;
+     servaddr.sin_addr.s_addr = inet_addr(serverIP);
+     servaddr.sin_port = htons(portNum);
+ 
+    // 연결요청
+     if(connect(listen_sock, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    {
+     perror("connect fail");
+     exit(0);
+    }
+
+        
+
+         if((fp = fopen(filename, "wb")) == 0)
+    {
+        printf("write file descriptor : FAILED\n");
+        exit(0);
+    }
+
+    //getting filesize
+    recv( listen_sock, buf, 4, 0);
+    memcpy(&filesize, (int*)buf,4);
+
+    printf("filename : %s, filesize : %d B\n", filename, filesize);
+
+
+
+        printf("processing : ");
+        while( total != filesize )
+        {
+            sread = recv( listen_sock, buf, BLOCK, 0 );
+//            printf( "file is receiving now.. " );
+            total += sread;
+            buf[sread] = 0;
+            fwrite( buf, 1, sread, fp);
+            bzero( buf, sizeof(buf) );
+            printf( "processing : %4.2f%% ", total*100 / (float)filesize );
+
+            
+        }
+
+        fclose(fp);
+        close(listen_sock);
+
+
+    (*threadIdx) = (*threadIdx) - 1;
+
+    return 0;
+}
+ 
+
+
+
+//파일을 보내는 함수 (아직 미구현 상태)
+
+ void* SendData(void *p)
+ {
+struct sockaddr_in servaddr;
+    int listen_sock, // 소켓번호
+    nbyte, nbuf;
+    char buf[FILE_BUFFER_SIZE];
+    char cli_ip[20];
+    char filename[20];
+    int filesize=0;
+    int total=0, sread;
+
+
+    FILE* fp;
+
+    char serverIP[MSGLEN] = {0, };
+
+    //unpack all datas
+    threadArgs* args = (threadArgs*)p;
+    int* threadIdx = NULL;
+    int portNum = args->portNum;
+    int BLOCK = args->speed; 
+
+    strcpy(filename, args->fname); //filename
+    strcpy(serverIP, args->serverIP);
+    threadIdx = args->threadIdx;
+
+    //nKB means 1024*n Bytes
+    BLOCK = 1024 * BLOCK;
+
+
+
+    if((listen_sock = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+    {
+     perror("socket fail");
+     exit(0);
+     }
+ 
+     // 에코 서버의 소켓주소 구조체 작성
+     bzero((char *)&servaddr, sizeof(servaddr));
+     servaddr.sin_family = AF_INET;
+     servaddr.sin_addr.s_addr = inet_addr(serverIP);
+     servaddr.sin_port = htons(portNum);
+ 
+    // 연결요청
+     if(connect(listen_sock, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    {
+     perror("connect fail");
+     exit(0);
+    }
+
+
+
+ if( (fp = fopen( filename, "rb" )) < 0 )
+ {
+ printf( "open failed" );
+ exit(0);
+ }
+ 
+ fseek( fp, 0L, SEEK_END );
+ filesize = ftell(fp);   //그 포인터 값이 바로 파일 크기이다.
+ fseek(fp, 0L, SEEK_SET );
+
+printf("filename : %s, filesize : %d B\n", filename, filesize);
+
+ send( listen_sock, &filesize, sizeof(filesize), 0 );
+ 
+ while( total != filesize )
+ {
+ sread = fread( buf, 1, BLOCK, fp );
+ printf( "file is sending now.. " );
+ total += sread;
+ buf[sread] = 0;
+ send( listen_sock, buf, sread, 0 );
+ printf( "processing :%4.2f%% ", total*100 / (float)filesize );
+ //        usleep(10000);
+ }
+ printf( "file translating is completed " );
+ printf( "filesize : %d, sending : %d ", filesize, total );
+ 
+ fclose(fp);
+ close(listen_sock);
+
+ (*threadIdx) = (*threadIdx) - 1;
+ return 0;
+
+}
--- server.c
+++ server.c
@@ -13,39 +13,37 @@
 #include <ctype.h>
 
 #define CLIENTS_NUM 5
-#define USABLE_THREADS 100
+#define USABLE_THREADS 500
 #define MSGLEN 128
-#define CMD_BUFFER_SIZE 128
 #define FILE_BUFFER_SIZE 2000
 
 //
-struct tranSpeed
+typedef struct tranSpeed
 {
     int putSpeed;
     int getSpeed;
-};
+} tranSpeed;
 
 //스레드를 실행할 때, 매개변수로 넣을 구조체.
 typedef struct threadArgs
 {
     char fname[MSGLEN];
     int speed;
-    int dataSocketDes;
-    struct sockaddr_in dataAddr;
-    socklen_t data_addr_size;
+    int portNum;
     int* threadIdx;
 } threadArgs;
 
-void ReceiveData();
-void SendData();
+void* ReceiveData(void* p);
+void* SendData(void* p);
 
 int main(int argc, char *argv[])
 {
-    struct tranSpeed speeds[100];         //speeds for per client
-    struct threadArgs args[100 * 10];   //arguments for thread
+    tranSpeed speeds[100];         //speeds for per client
+    threadArgs args[USABLE_THREADS];   //arguments for thread
+    pthread_t threads[USABLE_THREADS]; //threads
 
     int dataPort;         //데이터 전송용 포트는 명령어 포트에서 1씩 증가한 포트값을 사용
-    int threadIdx = 0;          //5개 스레드의 인덱스(스레드가 공유함)
+    int threadIdx = 0;          //스레드의 인덱스(스레드가 공유함)
     int threadIdxFIX;           //작업을 수행하다 인덱스가 바뀌면 안되니 고정
 
 
@@ -76,6 +74,7 @@ int main(int argc, char *argv[])
         perror("socket");
         exit(1);
     }
+
     if (isdigit(argv[1][0])) {
         static struct servent s;
         servp = &s;
@@ -84,6 +83,9 @@ int main(int argc, char *argv[])
         fprintf(stderr,"%s: unknown service\n", "tcp");
         exit(1);
     }
+
+    dataPort = atoi(argv[1]);
+
     memset((void *) &server, 0, sizeof server);
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
@@ -134,25 +136,61 @@ int main(int argc, char *argv[])
                 /* process the data */
                 bytesread = read(fd, buf, sizeof buf - 1);
                 buf[bytesread] = '\0';
-				//get [filename]이라면, filename 추출 후 파일을 클라이언트에게 보내준다.
-       	 if(strncmp(buf, "get", 3) == 0)
+			
+
+
+
+                        //get [filename]이라면, filename 추출 후 파일을 클라이언트에게 보내준다.
+       	                if(strncmp(buf, "get", 3) == 0)
 		        {
-            strcpy(fname, buf + 4); //파일명 추출
-            printf("[%s]을 보내주는 함수.\n", fname);
-            //------------send 함수 스레드로 실행 구현---------
+                                strcpy(fname, buf + 4); //파일명 추출
+                                printf("[%s]을 보내주는 함수.\n", fname);
+                                //------------send 함수 스레드로 실행 구현---------
+
+                                threadIdxFIX = threadIdx;
+                                strcpy(args[threadIdxFIX].fname, fname);
+                                args[threadIdxFIX].threadIdx = &threadIdx;
+
+                                threadIdx++;
+
+                                //now, we have to send new port num
+                                args[threadIdxFIX].portNum = ++dataPort;
+                                memcpy((int*)buf, &dataPort, 4);
+
+                                printf("new port for transfer : %d\n", args[threadIdxFIX].portNum);
+                                write(fd, buf, 4);
+
+                                pthread_create(&threads[threadIdxFIX], NULL, SendData, (void*)&args[threadIdxFIX]);
             
         		}
         
-        //put [filename]이라면, filename 추출 후 파일을 클라이언트에게서 받는다.
-   	     if(strncmp(buf, "put", 3) == 0)
-      		  {
-            strcpy(fname, buf + 4); //파일명 추출
-            printf("[%s]을 받는 함수.\n", fname);
-            //------------receive 함수 스레드로 실행 구현--------
-     		   }
+                        //put [filename]이라면, filename 추출 후 파일을 클라이언트에게서 받는다.
+   	                else if(strncmp(buf, "put", 3) == 0)
+        	       {
+                                 strcpy(fname, buf + 4); //파일명 추출
+                                 printf("[%s]을 받는 함수.\n", fname);
+                                //------------receive 함수 스레드로 실행 구현--------
+
+                                
+                                 threadIdxFIX = threadIdx;
+                                strcpy(args[threadIdxFIX].fname, fname);
+                                args[threadIdxFIX].threadIdx = &threadIdx;
+
+                                threadIdx++;
+
+                                //now, we have to send new port num
+                                args[threadIdxFIX].portNum = ++dataPort;
+                                memcpy((int*)buf, &dataPort, 4);
+
+                                printf("new port for transfer : %d\n", args[threadIdxFIX].portNum);
+                                write(fd, buf, 4);
+
+                                pthread_create(&threads[threadIdxFIX], NULL, ReceiveData, (void*)&args[threadIdxFIX]);
+                                
+     		          }
 
                 //sendrate (put)
-               if(strncmp(buf, "sendrate", 8) == 0)
+               else if(strncmp(buf, "sendrate", 8) == 0)
                {
                     //bring integer from buffer. As for"sendrate 40", value 40 begins at buffer[9].
                     strcpy(tempStr, buf + 9);  //extract number part
@@ -165,7 +203,7 @@ int main(int argc, char *argv[])
                }
 
             //recvrate (get)
-                if(strncmp(buf, "recvrate", 8) == 0)
+               else if(strncmp(buf, "recvrate", 8) == 0)
                {
                     //bring integer from buffer. As for"sendrate 40", value 40 begins at buffer[9].
                     strcpy(tempStr, buf + 9);  //extract number part
@@ -194,179 +232,197 @@ int main(int argc, char *argv[])
                 buf[bytesread] = '\0';
                 printf("%s: %d bytes from %d: %s\n",
                        argv[0], bytesread, fd, buf);
-                /* echo it back */
-                if (write(fd, buf, bytesread)!=bytesread)
-                    perror("echo");
+                
             }
         }
     }
 } /* main - server.c */
 
 
-/*
+
 //파일을 받는 함수 (아직 미구현 상태)
-void ReceiveData(char* portNum)
+void* ReceiveData(void* p)
 {
-    struct sockaddr_in servaddr, cliaddr;
-    int listen_sock, accp_sock, // 소켓번호
-    addrlen = sizeof(cliaddr), // 주소구조체 길이
-    nbyte, nbuf;
-    char buf[MSGLEN];
-    char cli_ip[20];
-    char filename[20];
-    int filesize=0;
-    int total=0, sread, fp;
-    
-//    // 소켓 생성
-//    if((listen_sock = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
-//        perror("socket fail");
-//        exit(0);
-//    }
-//    // servaddr을 ''으로 초기화
-//    bzero((char *)&servaddr, sizeof(servaddr));
-//    // servaddr 세팅
-//    servaddr.sin_family = AF_INET;
-//    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-//    servaddr.sin_port = htons(atoi(argv[1]));
-//    
-//    // bind() 호출
-//    if(bind(listen_sock, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
-//    {
-//        perror("bind fail");
-//        exit(0);
-//    }
-    
-    socketAndBind(&listen_sock, servaddr, portNum);
-    
-    // 소켓을 수동 대기모드로 세팅
-    listen(listen_sock, 5);
-    
-    while(1)
+     
+ struct sockaddr_in servaddr;
+ struct sockaddr_in cliaddr;
+int addrlen = sizeof(cliaddr);
+
+ int listen_sock, accp_sock, nbyte;
+ char buf[FILE_BUFFER_SIZE];
+ char filename[20];
+ int filesize, filenamesize ;
+
+ FILE* fp;
+ int sread, total=0;
+ 
+    //unpack all datas
+    threadArgs* args = (threadArgs*)p;
+    int* threadIdx = NULL;
+    int portNum = args->portNum;
+    int BLOCK = args->speed; 
+
+    strcpy(filename, args->fname); //filename
+    threadIdx = args->threadIdx;
+
+    //nKB means 1024*n Bytes
+    BLOCK = 1024 * BLOCK;
+
+ 
+     if((listen_sock = socket(PF_INET, SOCK_STREAM, 0)) < 0)
     {
-        puts("서버가 연결요청을 기다림..");
-        // 연결요청을 기다림
-        accp_sock = accept(listen_sock, (struct sockaddr *)&cliaddr, &addrlen);
-        
-        if(accp_sock < 0)
-        {
-            perror("accept fail");
-            exit(0);
-        }
-        
-        puts("클라이언트가 연결됨..");
-        
-        inet_ntop(AF_INET, &cliaddr.sin_addr.s_addr, cli_ip, sizeof(cli_ip));
-        printf( "IP : %s ", cli_ip );
-        printf( "Port : %x ", ntohs( cliaddr.sin_port) );
-        
-        bzero( filename, 20 );
-//        recv( accp_sock, filename, sizeof(filename), 0 );
-        read( accp_sock, filename, sizeof(filename) );
-        printf( "%s ", filename );
-        
-        // recv( accp_sock, &filesize, sizeof(filesize), 0 );
-        read( accp_sock, &filesize, sizeof(filesize) );
-        printf( "%d ", filesize );
-        
-        strcat( filename, "_backup" );
-        fp = open( filename, O_WRONLY | O_CREAT | O_TRUNC);
-        
+    perror("socket fail");
+    exit(0);
+    }
+ 
+ // 에코 서버의 소켓주소 구조체 작성
+ bzero((char *)&servaddr, sizeof(servaddr));
+ servaddr.sin_family = AF_INET;
+ servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+ //inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+ servaddr.sin_port = htons(portNum);
+ 
+    //bind() 호출
+    if(bind(listen_sock, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+   {
+        perror("bind fail");
+        exit(0);
+    }
+
+listen(listen_sock, 5);
+
+    accp_sock = accept(listen_sock, (struct sockaddr *)&cliaddr, &addrlen);
+
+    if((fp = fopen(filename, "wb")) == 0)
+    {
+        printf("write file descriptor : FAILED\n");
+        exit(0);
+    }
+
+    //getting filesize
+    recv( accp_sock, buf, 4, 0);
+    memcpy(&filesize, (int*)buf,4);
+
+    printf("filename : %s, filesize : %d B\n", filename, filesize);
+
+
+
         printf("processing : ");
         while( total != filesize )
         {
-            sread = recv( accp_sock, buf, 100, 0 );
+            sread = recv( accp_sock, buf, BLOCK, 0 );
 //            printf( "file is receiving now.. " );
             total += sread;
             buf[sread] = 0;
-            write( fp, buf, sread );
+            fwrite( buf, 1, sread, fp);
             bzero( buf, sizeof(buf) );
             printf( "processing : %4.2f%% ", total*100 / (float)filesize );
 
             
         }
-        printf( "file traslating is completed " );
-        printf( "filesize : %d, received : %d ", filesize, total );
-        
-        close(fp);
+
+        fclose(fp);
         close(accp_sock);
-    }
-    
-    close( listen_sock );
-    return 0;
+        close(listen_sock);
+
+        (*threadIdx) = (*threadIdx) - 1;
+        return 0;
+
+
 }
- */
+ 
 
 
 
 //파일을 보내는 함수 (아직 미구현 상태)
-/*
- void SendData()
+
+ void* SendData(void *p)
  {
  struct sockaddr_in servaddr;
- int s, nbyte;
- char buf[MAXLINE+1];
+ struct sockaddr_in cliaddr;
+int addrlen = sizeof(cliaddr);
+
+ int listen_sock, accp_sock, nbyte;
+ char buf[FILE_BUFFER_SIZE];
  char filename[20];
- int filesize, fp, filenamesize ;
+ int filesize, filenamesize ;
+
+ FILE* fp;
  int sread, total=0;
  
- if(argc != 3)
- {
- printf("usage: %s ip_address port ", argv[0]);
- exit(0);
- }
+    //unpack all datas
+    threadArgs* args = (threadArgs*)p;
+    int* threadIdx = NULL;
+    int portNum = args->portNum;
+    int BLOCK = args->speed; 
+
+    strcpy(filename, args->fname); //filename
+    threadIdx = args->threadIdx;
+
+    //nKB means 1024*n Bytes
+    BLOCK = 1024 * BLOCK;
+
  
- if((s = socket(PF_INET, SOCK_STREAM, 0)) < 0)
- {
- perror("socket fail");
- exit(0);
- }
+     if((listen_sock = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+    {
+    perror("socket fail");
+    exit(0);
+    }
  
  // 에코 서버의 소켓주소 구조체 작성
  bzero((char *)&servaddr, sizeof(servaddr));
  servaddr.sin_family = AF_INET;
- inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
- servaddr.sin_port = htons(atoi(argv[2]));
+ servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+ //inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+ servaddr.sin_port = htons(portNum);
  
- // 연결요청
- if(connect(s, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
- {
- perror("connect fail");
- exit(0);
- }
+    //bind() 호출
+    if(bind(listen_sock, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+   {
+        perror("bind fail");
+        exit(0);
+    }
+
+listen(listen_sock, 5);
+
+    accp_sock = accept(listen_sock, (struct sockaddr *)&cliaddr, &addrlen);
+
  
- printf("select file to send : ");
- if ( fgets(filename, sizeof(filename), stdin) == NULL )
- exit(0);
  
- filenamesize = strlen(filename);
- filename[filenamesize-1] = 0;
- 
- if( (fp = open( filename, O_RDONLY )) < 0 )
+ if( (fp = fopen( filename, "rb" )) < 0 )
  {
  printf( "open failed" );
  exit(0);
  }
  
- send( s, filename, sizeof(filename), 0 );
- 
- filesize = lseek( fp, 0, SEEK_END );
- send( s, &filesize, sizeof(filesize), 0 );
- lseek(fp, 0, SEEK_SET );
+ fseek( fp, 0L, SEEK_END );
+ filesize = ftell(fp);   //그 포인터 값이 바로 파일 크기이다.
+ fseek(fp, 0L, SEEK_SET );
+
+printf("filename : %s, filesize : %d B\n", filename, filesize);
+
+ send( accp_sock, &filesize, sizeof(filesize), 0 );
  
  while( total != filesize )
  {
- sread = read( fp, buf, 100 );
+ sread = fread( buf, 1, BLOCK, fp );
  printf( "file is sending now.. " );
  total += sread;
  buf[sread] = 0;
- send( s, buf, sread, 0 );
+ send( accp_sock, buf, sread, 0 );
  printf( "processing :%4.2f%% ", total*100 / (float)filesize );
  //        usleep(10000);
  }
  printf( "file translating is completed " );
  printf( "filesize : %d, sending : %d ", filesize, total );
  
- close(fp);
- close(s);
+ fclose(fp);
+ close(accp_sock);
+ close(listen_sock);
+
+ (*threadIdx) = (*threadIdx) - 1;
  return 0;
- }*/
+
+
+}
+
